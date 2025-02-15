410a102c2ecbbe5ff4ec8ebbc5e6fa537e5777cf
nickolasamaral03 <nickolas.amaral08@gmail.com>
2025-02-15 07:40:41 -0300

    subindo novo deploy

--- ApiPro/routes/auth.js
+++ ApiPro/routes/auth.js
@@ -85,35 +85,3 @@ router.post('/login', async(req, res) => {
 
 module.exports = router
 
-// ERRO AO CADASTRAR? (BUG) - VERIFICAR
-//  Problema era que: O .env estava de alguma forma invalidando nossa string de conexão, quando coloquei as credenciais direto na string funcionou.
-// Possivelmente não tem problema de rotas no projeto, mas o server.js tem que tá rodando junto com o front para ter acesso ao banco de dados e assim não ter problema com o axios.
-// Bug Atual: As informações estão indo para o banco de dados, tudo certo. Porém quando tento logar o usuario cadastrado, dar uma mensagem de "erro no servidor"
-// Verificar Bug
-// Parabéns pela evolução, Nickolas!
-
-// O .env não estava instalado apropriadamente e estava dando problema, agora está tudo certo.
-// Agora é lidar com o erro WebSocket e verificar como podemos dar o acesso ao usuario somente quando ele estiver logado.
-
-// Conseguimos deixar aparecer o header quando usuario está logado, utilizando o layout que possue somente as rotas diferentes de "/" e "/cadastro", as outras rotas aparece normalmente o header.
-// Precisaremos estilizar o login e cadastro ( devidamente estilizado e com logo e texto de apresentação)
-// Parabéns pela evolução, Nickolas!
-
-
-// Verificar pq os icones não estão aparecendo - URGENTE (agora estamos lidando com os arquivos do react no node e lidando com as rotas desconhecidas que carreguem no index.html e as rotas são diferentes 3000 e 5000)
-// Colocar mensagens bonitas e tirar os alerts
-// informar que as senhas devem ter nome e número
-
-
-// Tentar ver depois questão do websocket as vezes dá erro no console e as vezes não, verificar o que está acontecendo.
-// dar acesso somente com o login, sem outra manipulação (o acesso será somente tiver o token, quando o usuario não tiver não poderá manipular e ficará na parte de login)
-
-//
-
-// Organizar a tela para celulares
-
-//
-
-
-// Verificar também se está tudo certo segundo nosso Readme.md
-
